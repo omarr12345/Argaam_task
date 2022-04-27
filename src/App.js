4de1f24402fc917dd638fbf1ee467962e2a1e050
omarr12345 <omar12345omar15@gmail.com>
2022-04-27 17:26:00 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,391 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Almarai&family=Noto+Sans+Arabic:wght@100;200&family=Tajawal&display=swap');
+</style>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" dir="rtl">
+      <div className="widgets-container">
+        <div className="content">
+          <h2>القوائم الماليه</h2>
+
+          <table>
+            <tr className="t-row">
+              <th className="t-h">- التفاصيل</th>
+              <th className="t-h">مليون ريال</th>
+              <th className="t-h">مليون ريال</th>
+              <th className="t-h">مليون ريال</th>
+              <th className="t-h">مليون ريال</th>
+              <th className="t-h">مليون ريال</th>
+            </tr>
+
+            <tr className="position">
+              <td className="py-2">- قائمة الدخل</td>
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d " style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+
+            <tr className="t-row position">
+              <td className="py-2">- المصروفات</td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+
+            <tr className="t-row position">
+              <td className="py-2">- أرباح سنويه</td>
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+
+            <tr className="t-row position">
+              <td className="py-2">- جدول الاعمال</td>
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row position">
+              <td className="py-2">- قائمة المركز المالي </td>
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+            <tr className="t-row">
+              <td className="t-d" style={{ color: "white" }}>
+                الايرادات
+              </td>
+              <td className="t-d"> 125</td>
+              <td className="t-d"> 132</td>
+              <td className="t-d">115</td>
+              <td className="t-d"> 101</td>
+              <td className="t-d">12 </td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+<style>
+  @import
+  url('https://fonts.googleapis.com/css2?family=Almarai&family=Noto+Sans+Arabic:wght@100;200&family=Tajawal&display=swap');
+</style>;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
